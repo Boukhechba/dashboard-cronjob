ccf92a8b7ebb20d9724ccffe0e8b67db186437ef
Boukhechba <m_boukhechba@esi.dz>
2022-04-19 10:50:18 -0400

    Update data-ingest.py

--- data-ingest.py
+++ data-ingest.py
@@ -109,7 +109,7 @@ def count_probes(key,data):
                 subdata=data.loc[data['datum']== "ScriptState"]
                 grouped=pd.DataFrame(subdata.Id.groupby([subdata.State,(subdata.Timestamp.dropna().str[:13].replace("T", " ")),subdata.ParticipantId,subdata.DeviceId,subdata.OperatingSystem]).count()).reset_index()      
                 grouped=grouped.rename(columns={'State': 'datum'})
-                grouped.datum="ScriptDatum_"+grouped.datum
+                grouped.datum="ScriptDatum_"+str(grouped.datum)
                 frame=pd.concat([frame, grouped, grouped1], ignore_index=True)
             else:
                 frame=pd.concat([frame, grouped1], ignore_index=True)
